c8f057ea18b8cba436e30ea84661670e238fcb24
wac <wabaham9@gmail.com>
2023-05-23 19:10:09 +0300

    tasks 0-3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+''' basic flask app used to start a small localization program '''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from flask_babel import _
+from flask_babel import gettext as _l
+
+app = Flask(__name__)
+
+
+class Config:
+    ''' language and timezone configguration class '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config(['LANGUAGES']))
+
+
+@app.route('/')
+def home():
+    ''' serves 0-index.html '''
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
