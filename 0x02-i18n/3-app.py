c1fceaeea18a59cdeac8ed869ca4a6e8e55bcead
wac <wabaham9@gmail.com>
2023-05-26 09:41:25 +0300

    changes to task app 3 and 4

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config(['LANGUAGES']))
--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -18,6 +18,7 @@ class Config:
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
     return 'fr'#request.accept_languages.best_match(app.config(['LANGUAGES']))
